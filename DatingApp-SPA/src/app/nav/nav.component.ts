6ee4f7a0f2440e042fac1ff035400070cd407728
Shaon Dey <cseshaon@yahoo.com>
2019-03-10 22:25:03 +0600

    Added alertify for showing notification message

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,17 +10,17 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('logged in successfully');
+      this.alertify.success('logged in successfully');
     }, error => {
-      console.log(error);
-    })
+      this.alertify.error(error);
+    });
   }
 
   loggedIn() {
@@ -31,7 +32,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
 
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
