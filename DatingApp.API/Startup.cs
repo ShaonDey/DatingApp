5188b2f35e97e0b84a8ab44b56196f1ae36229e0
Shaon Dey <cseshaon@yahoo.com>
2019-03-12 22:17:25 +0600

    Added new repository for users

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -39,6 +39,7 @@ namespace DatingApp.API
             services.AddCors();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
