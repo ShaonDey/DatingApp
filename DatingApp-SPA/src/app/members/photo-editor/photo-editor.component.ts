556928b689cf66109200ecb6fd1311e35335942f
Shaon Dey <cseshaon@yahoo.com>
2019-03-19 21:40:10 +0600

    Added file uploader for uploading images

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
 import { Photo } from '../../_models/photo';
+import { environment } from '../../../environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,10 +11,30 @@ import { Photo } from '../../_models/photo';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
   }
 
 }
