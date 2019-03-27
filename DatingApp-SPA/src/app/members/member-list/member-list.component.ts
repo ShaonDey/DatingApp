6f4b732539b7ace16f31dfb39d26d02e6f96ee63
Shaon Dey <cseshaon@yahoo.com>
2019-03-27 21:55:35 +0600

    Added sorting functionality in SPA

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -28,6 +28,7 @@ export class MemberListComponent implements OnInit {
     this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
     this.userParams.minAge = 18;
     this.userParams.maxAge = 99;
+    this.userParams.OrderBy = 'lastActive';
   }
 
   pageChanged(event: any): void {
