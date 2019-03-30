2d6cf660d9c32b6adfee371111c461a687731bd4
Shaon Dey <cseshaon@yahoo.com>
2019-03-30 22:10:48 +0600

    Added retrieving list of users liked and liked by in API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -59,6 +59,18 @@ namespace DatingApp.API.Data
 
             users = users.Where(u => u.Gender.Equals(userParams.Gender));
 
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
+
             if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
                 var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
@@ -81,6 +93,20 @@ namespace DatingApp.API.Data
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users.Include(x => x.Likers).Include(x => x.Likees).FirstOrDefaultAsync(u => u.Id.Equals(id));
+
+            if (likers)
+            {
+                return user.Likers.Where(u => u.LikeeId.Equals(id)).Select(i => i.LikerId);
+            }
+            else
+            {
+                return user.Likees.Where(u => u.LikerId.Equals(id)).Select(i => i.LikeeId);
+            }
         }
 
         public async Task<bool> SaveAll()
