2a2b253a3c06711593310dbe97fc448db5e27aa1
Shaon Dey <cseshaon@yahoo.com>
2019-04-05 20:30:14 +0600

    Added image on message thread after sending message

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -91,7 +91,9 @@ namespace DatingApp.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
             {
                 return Unauthorized();
             }
@@ -111,10 +113,10 @@ namespace DatingApp.API.Controllers
 
                     _repo.Add(message);
 
-                    var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
-
                     if (await _repo.SaveAll())
                     {
+                        var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
+
                         return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
                     }
                     else
