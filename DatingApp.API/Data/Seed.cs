b45c80f9c80142aa30291a959b6887d1c431d81f
Shaon Dey <cseshaon@yahoo.com>
2019-04-10 20:44:52 +0600

    Updated seed class to insert initial data

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -15,24 +15,27 @@ namespace DatingApp.API.Data
 
         public void SeedUsers()
         {
-            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
-
-            var users = JsonConvert.DeserializeObject<List<User>>(userData);
-
-            foreach (var user in users)
+            if (!_context.Users.Any())
             {
-                byte[] passwordHash, passwordSalt;
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
 
-                CreatePasswordHash("password", out passwordHash, out passwordSalt);
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
 
-                user.PasswordHash = passwordHash;
-                user.PasswordSalt = passwordSalt;
-                user.Username = user.Username.ToLower();
+                foreach (var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
 
-                _context.Users.Add(user);
+                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+
+                    _context.Users.Add(user);
+                }
+
+                _context.SaveChanges();
             }
-
-            _context.SaveChanges();
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
